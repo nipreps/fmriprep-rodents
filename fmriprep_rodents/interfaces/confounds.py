9ca148a6c327c9612225fdec417c06e59ba245c9
Oscar Esteban <code@oscaresteban.es>
2021-01-13 14:17:50 +0100

    enh: final updates

--- fmriprep_rodents/interfaces/confounds.py
+++ fmriprep_rodents/interfaces/confounds.py
@@ -148,13 +148,11 @@ class ICAConfounds(SimpleInterface):
         return runtime
 
 
-<<<<<<< HEAD
 def _gather_confounds(
     signals=None,
     dvars=None,
     std_dvars=None,
-    fdisp=None,
-    rmsd=None,
+    fdisp=None,  # rmsd=None,
     tcompcor=None,
     acompcor=None,
     cos_basis=None,
@@ -162,11 +160,6 @@ def _gather_confounds(
     aroma=None,
     newpath=None,
 ):
-=======
-def _gather_confounds(signals=None, dvars=None, std_dvars=None, fdisp=None,  # rmsd=None,
-                      tcompcor=None, acompcor=None, cos_basis=None,
-                      motion=None, aroma=None, newpath=None):
->>>>>>> FIX: update confounds workflow
     r"""
     Load confounds from the filenames, concatenate together horizontally
     and save new file.
