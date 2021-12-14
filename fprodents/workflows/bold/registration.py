e480be901b0c0e1160f289d2a51834da898637fe
Eilidh MacNicol <eilidhmacnicol@gmail.com>
2021-12-14 09:52:00 +0000

    FIX: add fast basescale args

--- fprodents/workflows/bold/registration.py
+++ fprodents/workflows/bold/registration.py
@@ -108,7 +108,8 @@ Co-registration was configured with six degrees of freedom.
         FLIRTRPT(
             dof=bold2t1w_dof,
             generate_report=True,
-            uses_qform=True),
+            uses_qform=True,
+            args="-basescale 1"),
         name="coreg")
 
     if bold2t1w_init not in ("register", "header"):
