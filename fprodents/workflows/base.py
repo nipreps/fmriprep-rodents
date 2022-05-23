42cc759f2888aa36d6c9f13cef35f310771bc3f3
Eilidh MacNicol <eilidhmacnicol@gmail.com>
2022-05-23 15:42:42 +0100

    wip: add sdc wf into base wf

--- fprodents/workflows/base.py
+++ fprodents/workflows/base.py
@@ -284,21 +284,108 @@ were met (for participant <{subject_id}>, spaces <{', '.join(std_spaces)}>."""
     if anat_only:
         return workflow
 
+    from sdcflows import fieldmaps as fm
+    from sdcflows.interfaces.brainmask import BrainExtraction
+    fmap_estimators = None
+
+    if "fieldmaps" not in config.workflow.ignore:
+        # SDC Step 1: Run basic heuristics to identify available data for fieldmap estimation
+        # For now, no fmapless
+        base_entities = {
+            "subject": subject_id,
+            "extension": [".nii", ".nii.gz"],
+            "scope": "raw",  # Ensure derivatives are not captured
+        }
+
+        # Find fieldmap-less schemes
+        anat_file = config.execution.layout.get(suffix=["T2w", "T1w", "UNIT1"], **base_entities)
+        if not anat_file:
+            has_fieldmap = False
+            fmap_estimators = None
+        else:
+            from contextlib import suppress
+            from pathlib import Path
+            from sdcflows.utils.epimanip import get_trt
+            candidates = config.execution.layout.get(suffix="bold", **base_entities)
+            # Filter out candidates without defined PE direction
+            fmap_estimators = []
+            epi_targets = []
+            pe_dirs = []
+            ro_totals = []
+
+            for candidate in candidates:
+                meta = candidate.get_metadata()
+                pe_dir = meta.get("PhaseEncodingDirection")
+
+                if not pe_dir:
+                    continue
+
+                pe_dirs.append(pe_dir)
+                ro = 1.0
+                with suppress(ValueError):
+                    ro = get_trt(meta, candidate.path)
+                ro_totals.append(ro)
+                meta.update({"TotalReadoutTime": ro})
+                epi_targets.append(fm.FieldmapFile(candidate.path, metadata=meta))
+
+            for pe_dir in sorted(set(pe_dirs)):
+                pe_ro = [ro for ro, pe in zip(ro_totals, pe_dirs) if pe == pe_dir]
+                for ro_time in sorted(set(pe_ro)):
+                    fmfiles, fmpaths = tuple(
+                        zip(
+                            *[
+                                (target, str(Path(target.path)))
+                                for i, target in enumerate(epi_targets)
+                                if pe_dirs[i] == pe_dir and ro_totals[i] == ro_time
+                            ]
+                        )
+                    )
+                    fmap_estimators.append(
+                        fm.FieldmapEstimation(
+                            [
+                                fm.FieldmapFile(
+                                    anat_file[0], metadata={"IntendedFor": fmpaths}
+                                ),
+                                *fmfiles,
+                            ]
+                        )
+                    )
+
+            # fmap_estimators = [fm.FieldmapEstimation(
+            #     [str(f) for f in config.execution.layout.get(
+            #         return_type='file',
+            #         subject=subject_id,
+            #         suffix=['T2w', 'bold'],
+            #         extension='nii.gz')]
+            # )]
+
+        if config.workflow.use_syn_sdc and not fmap_estimators:
+            message = ("Fieldmap-less (SyN) estimation was requested, but "
+                       "PhaseEncodingDirection information appears to be "
+                       "absent.")
+            config.loggers.workflow.error(message)
+            if config.workflow.use_syn_sdc == "error":
+                raise ValueError(message)
+
+        if fmap_estimators:
+            config.loggers.workflow.info(
+                "B0 field inhomogeneity map will be estimated with "
+                f" the following {len(fmap_estimators)} estimators: "
+                f"{[e.method for e in fmap_estimators]}."
+            )
+
     # Append the functional section to the existing anatomical exerpt
     # That way we do not need to stream down the number of bold datasets
-    anat_preproc_wf.__postdesc__ = (
-        (anat_preproc_wf.__postdesc__ or "")
-        + """
+    func_pre_desc = """
 
 Functional data preprocessing
 
 : For each of the {num_bold} BOLD runs found per subject (across all
 tasks and sessions), the following preprocessing was performed.
-""".format(
-            num_bold=len(subject_data["bold"])
-        )
-    )
+""".format(num_bold=len(subject_data["bold"]))
 
+    func_preproc_wfs = []
+    has_fieldmap = bool(fmap_estimators)
     for bold_file in subject_data["bold"]:
         echoes = extract_entities(bold_file).get("echo", [])
         echo_idxs = listify(echoes)
@@ -324,7 +411,13 @@ tasks and sessions), the following preprocessing was performed.
         bold_ref_wf.inputs.n4_avgs.shrink_factor = 1
         bold_ref_wf.inputs.n4_avgs.n_iterations = [50] * 4
 
-        func_preproc_wf = init_func_preproc_wf(bold_file)
+        skullstrip_ref = pe.Node(BrainExtraction(), name='skullstrip_ref')
+
+        func_preproc_wf = init_func_preproc_wf(bold_file, has_fieldmap=has_fieldmap)
+        if func_preproc_wf is None:
+            continue
+
+        func_preproc_wf.__desc__ = func_pre_desc + (func_preproc_wf.__desc__ or "")
 
         # fmt:off
         workflow.connect([
@@ -341,8 +434,69 @@ tasks and sessions), the following preprocessing was performed.
               ('outputnode.xfm_files', 'inputnode.bold_ref_xfm'),
               ('outputnode.validation_report', 'inputnode.validation_report'),
               (('outputnode.n_dummy', _pop), 'inputnode.n_dummy_scans')]),
+            (bold_ref_wf, skullstrip_ref, [('outputnode.epi_ref_file', 'in_file')])
         ])
         # fmt:on
+        func_preproc_wfs.append(func_preproc_wf)
+
+    if not has_fieldmap:
+        return workflow
+
+    from sdcflows.workflows.base import init_fmap_preproc_wf
+
+    # Select "Fischer344" from standard references.
+    create_prior = pe.Node(niu.Function(function=_create_atlas_prior), name="create_prior")
+    create_prior.inputs.template = 'Fischer344'
+    create_prior.inputs.suffix = 'T2w'
+
+    fmap_wf = init_fmap_preproc_wf(
+        debug=config.execution.debug,
+        estimators=fmap_estimators,
+        omp_nthreads=config.nipype.omp_nthreads,
+        output_dir=output_dir,
+        subject=subject_id,
+    )
+    fmap_wf.__desc__ = f"""
+
+Preprocessing of B<sub>0</sub> inhomogeneity mappings
+
+: A total of {len(fmap_estimators)} fieldmaps were found available within the input
+BIDS structure for this particular subject.
+"""
+
+    # Overwrite ``out_path_base`` of sdcflows's DataSinks
+    for node in fmap_wf.list_node_names():
+        if node.split(".")[-1].startswith("ds_"):
+            fmap_wf.get_node(node).interface.out_path_base = ""
+
+    refmap_metadata = pe.Node(niu.Function(function=_merge_meta), name="refmap_metadata")
+    source_meta = [s.metadata for s in _pop(fmap_estimators).sources if s.suffix == 'bold']
+    refmap_metadata.inputs.meta_list = source_meta
+
+    # fmt: off
+    workflow.connect([
+        (anat_preproc_wf, fmap_wf, [
+            ("outputnode.t2w_preproc", f"in_{_pop(fmap_estimators).bids_id}.anat_ref"),
+            ("outputnode.t2w_mask", f"in_{_pop(fmap_estimators).bids_id}.anat_mask")]),
+        (bold_ref_wf, refmap_metadata, [('outputnode.epi_ref_file', 'epi_ref')]),
+        (refmap_metadata, fmap_wf, [
+            ("out", f"in_{_pop(fmap_estimators).bids_id}.epi_ref")]),
+        (skullstrip_ref, fmap_wf, [('out_mask', f"in_{_pop(fmap_estimators).bids_id}.epi_mask")]),
+        (create_prior, fmap_wf, [("out", f"in_{_pop(fmap_estimators).bids_id}.sd_prior")])
+    ])
+
+    for func_preproc_wf in func_preproc_wfs:
+        workflow.connect([
+            (fmap_wf, func_preproc_wf, [
+                ("outputnode.fmap", "inputnode.fmap"),
+                ("outputnode.fmap_ref", "inputnode.fmap_ref"),
+                ("outputnode.fmap_coeff", "inputnode.fmap_coeff"),
+                ("outputnode.fmap_mask", "inputnode.fmap_mask"),
+                ("outputnode.fmap_id", "inputnode.fmap_id"),
+                ("outputnode.method", "inputnode.sdc_method"),
+            ]),
+        ])
+    # fmt: on
     return workflow
 
 
@@ -354,3 +508,33 @@ def _pop(inlist):
     if isinstance(inlist, (list, tuple)):
         return inlist[0]
     return inlist
+
+
+def _create_atlas_prior(template=None, suffix=None):
+    from templateflow.api import get
+    import nibabel as nb
+    import numpy as np
+    from pathlib import Path
+    from nipype.utils.filemanip import fname_presuffix
+
+    if template is None:
+        template = 'Fischer344'
+    if suffix is None:
+        suffix = 'T2w'
+
+    tpl_path = get(template, suffix=suffix)
+    tpl_img = nb.load(tpl_path)
+    tpl_data = tpl_img.get_fdata()
+    new_data = np.ones_like(tpl_data) * 4
+
+    out = fname_presuffix(Path(tpl_path).name, suffix="_sdcprior", newpath=Path.cwd())
+    hdr = tpl_img.header.copy()
+    hdr.set_data_dtype("uint8")
+    tpl_img.__class__(new_data.astype("uint8"), tpl_img.affine, hdr).to_filename(out)
+
+    return out
+
+
+def _merge_meta(epi_ref, meta_list):
+    """Prepare a tuple of EPI reference and metadata."""
+    return (epi_ref, meta_list[0])
